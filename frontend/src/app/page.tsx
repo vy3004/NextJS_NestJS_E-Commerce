23d318db88da69cd38180941700fcd552a154f81
Kha Vy <vytrannguyen3004@gmail.com>
2024-09-07 01:22:58 +0700

    init base frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
+      <Button variant="ghost">Button</Button>
+    </>
+  );
+}
